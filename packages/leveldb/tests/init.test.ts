cc81634d94df013af8b4a56b1c8d720634c89ddf
mixmix <mix@protozoa.nz>
2024-07-04 09:39:19 +1200

    write basic test

--- packages/leveldb/tests/init.test.ts
+++ packages/leveldb/tests/init.test.ts
@@ -10,8 +10,8 @@ describe("Testing suite", () => {
       describe, it, beforeEach, afterEach
     }, {
       name: 'leveldb',
-      getStorage() {
-        return createLevelDBStorage({ dbPath: './db' })
+      getStorage(i = '') {
+        return createLevelDBStorage({ dbPath: './db' + i })
       },
       getPerformanceStorage() {
         return {
