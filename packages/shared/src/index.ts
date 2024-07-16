cac0c8ede17b0793d1f10d9bf89b6d509684847b
chereseeriepa <cherese.eriepa@outlook.com>
2024-07-16 13:31:27 +1200

    update bound methods to match rxdb implementations

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -38,14 +38,14 @@ export function compareDocsWithIndex<RxDocType>(
   a: DocWithIndexString<RxDocType>,
   b: DocWithIndexString<RxDocType>
 ): 1 | 0 | -1 {
-  if (a.indexString === undefined) throw Error(`missing indexString on ${a}`)
-  if (b.indexString === undefined) throw Error(`missing indexString on ${b}`)
-  if (a.indexString < b.indexString) {
-    return -1
-  } else if (a.indexString === b.indexString) {
-    return 0
+  const indexStringA = a[0];
+  const indexStringB = b[0];
+  if (indexStringA < indexStringB) {
+      return -1;
+  } else if (indexStringA === indexStringB) {
+      return 0;
   } else {
-    return 1
+      return 1;
   }
 }
 
@@ -119,103 +119,101 @@ export function conditionMatches<RxDocType>(selector: MangoQuerySelector<RxDocTy
   return false
 }
 
-type Compare<T> = ((a: T, b: T) => number | null | undefined)
+type Compare<T> = ((a: T, b: T) => number | null | undefined);
 
 function ge<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any): number {
-  let i: number = h + 1
-  while (l <= h) {
-    const m = (l + h) >>> 1
-    const x: any = a[m]
-    const p: any = (c !== undefined) ? c(x, y) : (x - (y as any))
-    if (p >= 0) {
-      i = m; h = m - 1
-    } else {
-      l = m + 1
+    let i: number = h + 1;
+    while (l <= h) {
+        const m = (l + h) >>> 1;
+        const x: any = a[m];
+        const p: any = (c !== undefined) ? c(x, y) : (x - (y as any));
+        if (p >= 0) {
+            i = m; h = m - 1;
+        } else {
+            l = m + 1;
+        }
     }
-  }
-  return i
+    return i;
 }
 
 function gt<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any): number {
-  let i = h + 1
+  let i = h + 1;
   while (l <= h) {
-    const m = (l + h) >>> 1
-    const x = a[m]
-    const p: any = (c !== undefined) ? c(x!, y) : ((x as any) - (y as any))
-    if (p > 0) {
-      i = m; h = m - 1
-    } else {
-      l = m + 1
-    }
+      const m = (l + h) >>> 1;
+      const x = a[m];
+      const p: any = (c !== undefined) ? c(x!, y) : ((x as any) - (y as any));
+      if (p > 0) {
+          i = m; h = m - 1;
+      } else {
+          l = m + 1;
+      }
   }
-  return i
+  return i;
 }
 
 function lt<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any): number {
-  let i = l - 1
+  let i = l - 1;
   while (l <= h) {
-    const m = (l + h) >>> 1; const x = a[m]
-    const p: any = (c !== undefined) ? c(x!, y) : ((x as any) - (y as any))
-    if (p < 0) {
-      i = m; l = m + 1
-    } else {
-      h = m - 1
-    }
+      const m = (l + h) >>> 1, x = a[m];
+      const p: any = (c !== undefined) ? c(x!, y) : ((x as any) - (y as any));
+      if (p < 0) {
+          i = m; l = m + 1;
+      } else {
+          h = m - 1;
+      }
   }
-  return i
+  return i;
 }
 
 function le<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any): number {
-  let i = l - 1
+  let i = l - 1;
   while (l <= h) {
-    const m = (l + h) >>> 1; const x = a[m]
-    const p: any = (c !== undefined) ? c(x!, y) : ((x as any) - (y as any))
-    if (p <= 0) {
-      i = m; l = m + 1
-    } else {
-      h = m - 1
-    }
+      const m = (l + h) >>> 1, x = a[m];
+      const p: any = (c !== undefined) ? c(x!, y) : ((x as any) - (y as any));
+      if (p <= 0) {
+          i = m; l = m + 1;
+      } else {
+          h = m - 1;
+      }
   }
-  return i
+  return i;
 }
 
 function eq<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any): number {
   while (l <= h) {
-    const m = (l + h) >>> 1; const x = a[m]
-    const p: any = (c !== undefined) ? c(x!, y) : ((x as any) - (y as any))
-    if (p === 0) {
-      return m
-    }
-    if (p <= 0) {
-      l = m + 1
-    } else {
-      h = m - 1
-    }
+      const m = (l + h) >>> 1, x = a[m];
+      const p: any = (c !== undefined) ? c(x!, y) : ((x as any) - (y as any));
+      if (p === 0) {
+          return m;
+      }
+      if (p <= 0) {
+          l = m + 1;
+      } else {
+          h = m - 1;
+      }
   }
-  return -1
+  return -1;
 }
 
 function norm<T>(a: T[], y: T, c: Compare<T>, l: any, h: any, f: any) {
-  if (typeof c === 'function') {
-    return f(a, y, c, (l === undefined) ? 0 : l | 0, (h === undefined) ? a.length - 1 : h | 0)
-  }
-  return f(a, y, undefined, (c === undefined) ? 0 : c | 0, (l === undefined) ? a.length - 1 : l | 0)
+  return f(a, y, c, (l === undefined) ? 0 : l | 0, (h === undefined) ? a.length - 1 : h | 0);
 }
 
+
 export function boundGE<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any) {
-  return norm(a, y, c, l, h, ge)
+  return norm(a, y, c, l, h, ge);
 }
 export function boundGT<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any) {
-  return norm(a, y, c, l, h, gt)
+  return norm(a, y, c, l, h, gt);
 }
 export function boundLT<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any) {
-  return norm(a, y, c, l, h, lt)
+  return norm(a, y, c, l, h, lt);
 }
 export function boundLE<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any) {
-  return norm(a, y, c, l, h, le)
+  return norm(a, y, c, l, h, le);
 }
 export function boundEQ<T>(a: T[], y: T, c: Compare<T>, l?: any, h?: any) {
-  return norm(a, y, c, l, h, eq)
+  return norm(a, y, c, l, h, eq);
 }
 
 export function fixTxPipe(str: string): string {
